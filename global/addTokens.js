127095c1f5e055fbb22455a717b16925ebdb04bc
matteew343 <mac1casarez1@gmail.com>
2025-04-07 02:23:53 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -50,8 +50,8 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
-                            addedXp: 300
+                            addedTokens: 10000,
+                            addedXp: 500
                         }, e.secret)
                     }).then(() => alert('Added daily rewards!')).catch(() => alert('There was an error when adding rewards!'));;
                 }).catch(() => alert('There was an error encoding requests!'));
